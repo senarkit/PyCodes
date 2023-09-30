98360c0011eda89db0cf4d970cd87a9791869f1e
Arkit Sen <senarkit@gmail.com>
2023-09-30 17:39:47 +0530

    TimeSeries-Practice
    check MA

--- Projects/TimeSeries-Vandeput/MA.py
+++ Projects/TimeSeries-Vandeput/MA.py
@@ -7,6 +7,8 @@ def moving_average(d, extra_periods=1, n=3):
     to calculate moving average
     Parameters:
         d : a time series that contains the historical demand
+        n : (int) lag window
+        extra_periods : (int) no of period intend to predict
     Returns:
         dataframe
     """
@@ -20,11 +22,13 @@ def moving_average(d, extra_periods=1, n=3):
     f = np.full(cols + extra_periods, np.nan)
 
     # create all t+1 forecasts
-    for t in range(n, cols + 1):
+    for t in range(n, cols + extra_periods):
         f[t] = round(np.mean(d[t-n: t]), 2)
+        if np.isnan(d[t]):
+            d[t] = round(np.mean(d[t-n: t]), 2)
 
     # forecast for all extra periods
-    f[cols+1:] = f[t]
+    d[cols:] = np.nan
     df = pd.DataFrame.from_dict({"Demand": d, "Forecast": f, "Error": d - f})
 
     return df
