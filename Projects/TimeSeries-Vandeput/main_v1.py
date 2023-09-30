2e91209bf1fd3d1091f4115f8c66138b21901255
Arkit Sen <senarkit@gmail.com>
2023-09-30 17:41:38 +0530

    TimeSeries-Vandeput Updates (#7)
    * Folder name changes
    * TimeSeries-Practice

--- Projects/TimeSeries-Vandeput/main_v1.py
+++ Projects/TimeSeries-Vandeput/main_v1.py
@@ -1,5 +1,7 @@
 import MA
+import pandas as pd
 
 d = [28, 19, 18, 13, 19, 16, 19, 18, 13, 16, 16, 11, 18, 15, 13, 15, 13, 15, 13, 11, 13, 10, 12]
-df = MA.moving_average(d)
-print(df)
+d = [28, 19, 18, 13, 19, 16]
+df = MA.moving_average(d, extra_periods=5, n=3)
+print(df])
