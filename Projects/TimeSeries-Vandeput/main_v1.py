5ab4d0fbbefffebf1196f693fb7843fb46417838
Arkit Sen <senarkit@gmail.com>
2023-10-01 17:43:41 +0530

    Update OOT in MA
    update the forecast part in Moving Average

--- Projects/TimeSeries-Vandeput/main_v1.py
+++ Projects/TimeSeries-Vandeput/main_v1.py
@@ -1,9 +1,10 @@
 import MA
 import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 ###################### CONFIG ####################
 st_date = "2015-01-01"
-en_date = "2017-12-01"
+en_date = "2016-12-01"
 
 ###################### CODE #####################
 ## Get Data
@@ -14,6 +15,19 @@ data = data[(data.Date >= pd.to_datetime(st_date).date()) &
 data = data.set_index('Date')
 
 ## Movign Average
-df = MA.moving_average(data.Demand, extra_periods=5, n=3)
+df = MA.moving_average(data.Demand, extra_periods=6, n=3)
 
-print(df)
+## Exponential Smoothing
+
+
+#### OOT Period
+pred_data = df[df.index > pd.to_datetime(en_date).date()]
+oot_data = pd.read_csv("Projects\TimeSeries-Vandeput\data\monthly_test.csv")
+oot_data.Date = pd.to_datetime(oot_data['Date']).apply(lambda x: x.date())
+
+pred_data.index.name = 'Date'
+pred_data = pred_data.reset_index()[['Date','Forecast']].merge(oot_data, on='Date', how='left')
+pred_data['Error'] = round((pred_data['Forecast'] - pred_data['Demand'])/ pred_data['Demand'] *100, 2)
+
+print("MAE :", {mean_absolute_error(pred_data["Demand"], pred_data["Forecast"])})
+print("MSE :", {mean_squared_error(pred_data["Demand"], pred_data["Forecast"])})
